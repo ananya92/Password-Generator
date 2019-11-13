6dc7fd8bb12f81c877a2def5e08f87524919691a
ananya92 <1992ananya@gmail.com>
2019-11-13 16:25:56 +1030

    The user input prompts were getting fired before the html page was loaded. Fixed this by using the onload method of window object to run js code after the window loads

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-var passwordLength;             //Variable to store length of password
-var hasSpecialChars;            //Variable to store if special charaters are required in password
-var hasNumericChars;            //Variable to store if numeric charaters are required in password
-var hasLowercaseChars;          //Variable to store if lowercase charaters are required in password
-var hasUppercaseChars;          //Variable to store if uppercase charaters are required in password
+window.onload = function() {
+    var passwordLength;             //Variable to store length of password
+    var hasSpecialChars;            //Variable to store if special charaters are required in password
+    var hasNumericChars;            //Variable to store if numeric charaters are required in password
+    var hasLowercaseChars;          //Variable to store if lowercase charaters are required in password
+    var hasUppercaseChars;          //Variable to store if uppercase charaters are required in password
 
-passwordLength = prompt("Please enter the length of password to be generated");
+    passwordLength = prompt("Please enter the length of password to be generated");
 
-if((passwordLength != null) && (passwordLength > 7) && (passwordLength < 129)) {
-    hasSpecialChars = confirm("Do you want special charaters?");
-    hasNumericChars = confirm("Do you want numbers?");
-    hasLowercaseChars = confirm("Do you want lowercase alphabets?");
-    hasUppercaseChars = confirm("Do you want uppercase alphabets?")
+    if((passwordLength != null) && (passwordLength > 7) && (passwordLength < 129)) {
+        hasSpecialChars = confirm("Do you want special charaters?");
+        hasNumericChars = confirm("Do you want numbers?");
+        hasLowercaseChars = confirm("Do you want lowercase alphabets?");
+        hasUppercaseChars = confirm("Do you want uppercase alphabets?")
+    }
+    else {
+        alert("Please enter length between 8 and 128");
+    }
 }
-else {
-    alert("Please enter length between 8 and 128");
-}
-
